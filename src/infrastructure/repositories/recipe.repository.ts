33dd25ed517baa103f7b94c8771fbce7b8325a3d
AgusLasalvia <agus.blumenfeld13@gmail.com>
2025-04-13 17:28:19 -0300

    fix: home recipe recomendation not random

--- src/infrastructure/repositories/recipe.repository.ts
+++ src/infrastructure/repositories/recipe.repository.ts
@@ -1,4 +1,5 @@
 import * as dto from "../../app/dto/recipe.dto";
+import { Prisma } from "@prisma/client";
 import prisma from "../database/prisma";
 
 export class RecipeRepository {
@@ -6,7 +7,6 @@ export class RecipeRepository {
 	static async findById(id: string): Promise<any | null> {
 		throw new Error("Method not implemented.");
 	}
-
 
 	static async create(recipe: dto.CreateRecipeDto): Promise<dto.CreateRecipeDto | null> {
 		const newRecipe = await prisma.recipe.create({
@@ -16,24 +16,19 @@ export class RecipeRepository {
 				instructions: recipe.instructions,
 				img_url: recipe.img_url,
 				user_id: recipe.user_id,
-				category_id: recipe.category_id, // Ensure categoryId is provided
+				category_id: recipe.category_id,
 			},
 		});
 		return newRecipe;
 	}
 
 	static async getRandomRecipes(): Promise<dto.RandomRecipesDto[] | []> {
-		const recipes = await prisma.recipe.findMany({
-			select: {
-				id: true,
-				title: true,
-				img_url: true,
-			},
-			take: 6, // Limit to 6 random recipes
-			orderBy: {
-				id: 'asc', // Order by id in ascending order
-			},
-		});
+		const recipes = await prisma.$queryRaw<dto.RandomRecipesDto[]>(Prisma.sql`
+			SELECT id, title, img_url
+			FROM "Recipe"
+			ORDER BY RANDOM()
+			LIMIT 6
+		`);
 
 		return recipes.length > 0 ? recipes : [];
 	}
